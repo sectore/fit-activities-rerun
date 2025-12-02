8beb280ff7bf1a3321190b7bc6915b3774914525
jk <47693+sectore@users.noreply.github.com>
2025-12-02 20:28:10 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "fit-activities-rerun";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShells.default = mkShell rec {
+          buildInputs =
+            [
+              # Python
+              python3
+              uv
+
+              # misc. libraries
+              pkg-config
+              zlib
+
+              # nix
+              nixd
+              alejandra
+
+              freetype
+
+              # misc
+              typos
+              just
+            ]
+            ++ lib.optionals stdenv.isLinux [
+              # GUI libs
+              libxkbcommon
+              libGL
+              fontconfig
+
+              # wayland libraries
+              wayland
+
+              # graphics and vulkan
+              mesa
+              vulkan-loader
+
+              # x11 libraries
+              xorg.libXcursor
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libX11
+            ];
+
+          shellHook = ''
+            # Set LD_LIBRARY_PATH first for compiled packages
+            export LD_LIBRARY_PATH=${lib.makeLibraryPath buildInputs}:${lib.makeLibraryPath [stdenv.cc.cc]}:$LD_LIBRARY_PATH
+
+            # Create virtual environment if it doesn't exist
+            if [ ! -d ".venv" ]; then
+                uv sync
+            fi
+            source .venv/bin/activate
+            uv sync
+
+          '';
+        };
+      });
+}
